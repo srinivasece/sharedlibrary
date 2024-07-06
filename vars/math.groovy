4076d6b53838c2fcca91851ed9c399991aa5d6fa
srinivasece <jsrinivasarao38@gmail.com>
2024-07-06 13:06:36 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,17 @@
+def sum(a,b)
+{
+  echo "sum of $a and $b is $(a+b)"
+}
 
+def sub(a,b)
+{
+  echo "substract of $a and $b is $(a-b)"
+}
+def mul(a,b)
+{
+  echo "product of $a and $b is $(a*b)"
+}
+def div(a,b)
+{
+  echo "division of $a and $b is $(a/b)"
+}
